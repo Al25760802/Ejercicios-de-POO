60a4a36a6ab445d3168c758c84dde391debf2c4a
Al25760802 <al25760802@ite.edu.mx>
2026-03-20 20:34:29 -0700

    Delete Ejercicio1/Ejercicio5A directory

--- Ejercicio1/Ejercicio5A/PROBLEMA1/Main.py
+++
@@ -1,8 +0,0 @@
-if __name__ == "__main__":
-    mobs = [
-        Vaca("Bessie", 10),
-        Creeper("Explosi", 20),
-        Enderman("Tall Boi", 40),
-    ]
-    for mob in mobs:
-        mob.presentarse()
